3fff0ff5bb9909a6277d63cda113ab5ae145f213
felipe <felipemoreno148@gmail.com>
2024-11-19 19:18:49 -0300

    Adiciona a header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,16 @@ import logo from '../assets/logo.svg'
 function Home() {
   return (
     <div>
-
+      <div id='headerContainer'>
+          <header>
+            <img src={logo} id='logo'/>
+            <ul typeof=''>
+              <li><a href="">Sobre nós</a></li>
+              <li><a href="">Portifolio</a></li>
+              <li><a href="">Contato</a></li>
+            </ul>
+          </header>
+      </div>
     </div>
   )
 }
